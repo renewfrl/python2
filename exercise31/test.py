0478e5b06f5a2603be838c38f9e6c5f153fdfaed
work <work@MacBook-Pro.fritz.box>
2019-05-06 08:32:35 +0200

    bogus data

--- exercise31/test.py
+++
@@ -1,7 +0,0 @@
-import csv
-with open('test.csv') as csvfile:
-    reader = csv.reader(csvfile, delimiter=';')
-    for row in reader:
-        # lazy solution-first3 should normally iterate for the rows or put the rows in a dictionary
-        print(row[0])
-        print(row[1])
