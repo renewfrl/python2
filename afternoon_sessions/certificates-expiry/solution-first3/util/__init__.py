61abd0a8215ad6c8d90e8da40c7d9cfe492b8d2d
work <work@MacBook-Pro.lan>
2019-05-06 15:38:45 +0200

    time function

--- afternoon_sessions/certificates-expiry/solution-first3/util/__init__.py
+++ afternoon_sessions/certificates-expiry/solution-first3/util/__init__.py
@@ -1,1 +1,15 @@
+from datetime import datetime
 
+def number_of_days_till_expire(ssl_date):
+
+    try:
+        expire_date = datetime.strptime(ssl_date,
+                                        "%b %d %H:%M:%S %Y %Z")
+    except Exception as e:
+        raise Exception(e)
+
+    expire_in = expire_date - datetime.now()
+    if expire_in.days > 0:
+        return expire_in.days
+    else:
+        return False
